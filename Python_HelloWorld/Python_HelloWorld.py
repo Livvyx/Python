ea1aa2708eb91dcafac3e27095d1d20f95e164b6
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2021-12-08 09:50:49 +1300

    Add files via upload

---
+++ Python_HelloWorld/Python_HelloWorld.py
@@ -0,0 +1,7 @@
+
+
+#outputting a statement/Question.
+print ("Hello, Which One Is Greater Five Or Two?!")
+
+if 5 > 2:
+    print ("Five is Greater Than Two!")
---
+++ Python_Variables/Python_Variables.py
@@ -0,0 +1,24 @@
+
+
+#Python has no command for declaring a variable
+
+n = "---- Variables being declared ----"
+
+p = 5
+
+y = "Olivia"
+print(n)
+
+print(p)
+
+print(y)
+
+#Variables do not need to be declared with any particular type, and can even change type after they have been set.
+
+#EXAMPLE
+k = "---- Variables being declared ----"
+x = 4 # x is of the type int (integer)
+x = "Olivia" # x is now of type str (string)
+print(k)
+print(x)
+
