696697c7f2dc42409030fec7776a5cbb178b5638
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2021-12-08 09:55:00 +1300

    Add files via upload

---
+++ Python_Casting/Python_Casting.py
@@ -0,0 +1,15 @@
+#Olivia Walker
+#Python Coding Practice
+#Topic: Casting 
+
+a = str(1) # a will be a '1'
+
+b = int(2) # b will be a 2
+
+c = float(3) # c will be a 3.0
+
+print(a)
+print(b)
+print(c)
+
+
