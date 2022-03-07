fdcde18a6b11bb90dd84a96c256da1f20b3428c6
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2022-03-08 09:24:48 +1300

    Add files via upload

--- Python_StandardLibrary.py
+++ Python_StandardLibrary.py
@@ -1,2 +1,23 @@
-import math
-print(math.pi)
+"""
+Olivia Walker
+CS202 Python
+Playing around with standard library outputs/inputs
+"""
+import math  # inputting standard library - math
+import random
+
+print("\n* * * * * * Math Standard Library * * * * * * ")
+print('\n')
+print('\n * pi * ')
+print(math.pi)  # ouputting pi number
+print('\n * SquareRoot * ')
+print(math.sqrt(144))  # ouputting square-root
+print('\n * Sin * ')
+print(math.sin(30))
+
+print("\n* * * * * * Random Standard Library * * * * * * ")
+print('\n')
+print('\n * Random integer * ')
+print(random.randint(10, 30))
+print('\n * Random range * ')
+print(random.randrange(10, 40, 2))
