3bbab1beb64e4d361caccc3be23a0be883227931
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2022-03-08 09:39:57 +1300

    Add files via upload

---
+++ Python_ModuleFunctions.py
@@ -0,0 +1,13 @@
+"""
+Olivia Walker
+CS202 Python
+functions & Modules
+"""
+import math
+import random
+import Python_ReturnFunctions  # importing from the module file ReturnFunctions
+
+print("\n* * * * * * Calling the function GreaterNumber * * * * * * ")
+print(Python_ReturnFunctions.GreaterNumber(12, 37))  # Calling the function from the module ReturnFunctions
+print("\n* * * * * * Calling the function SmallerNumber * * * * * * ")
+print(Python_ReturnFunctions.SmallerNumber(110, 1500))
--- Python_ReturnFunctions.py
+++ Python_ReturnFunctions.py
@@ -4,8 +4,13 @@ CS202 Python
 Return Functions
 """
 def GreaterNumber(num1, num2):
-    if(num1>num2):
-        return num1
-    else:
-        return num2
-print(GreaterNumber(54,36), ' is the greatest')  # returning the values/callng the function
+    if(num1>num2):  # if num1 is more then num2
+        return num1  # return if num1 is more then num 2
+    else:  # if num1 isnt more then num2
+        return num2  # return num2 if more then num1
+
+def SmallerNumber(num3, num4):
+    if(num3<num4):  # if num3 is less than num4
+        return num3  # return num3
+    else:  # if num3 is not less then num4
+        return num4  # return num4
