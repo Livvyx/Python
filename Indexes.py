ac0debc78482c4e0f1a4a137b3e9289a1224b895
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2022-04-05 09:43:57 +1200

    Add files via upload

---
+++ Indexes.py
@@ -0,0 +1,16 @@
+"""
+Olivia Walker
+CS202
+
+indexes
+"""
+
+# creating a list
+numbers = [2, 5, "Olivia", True, 9, 5.4, 3, 7]
+
+# defining even numbers
+evenNumbers = [2, 4, 6, 8, 10, 12]
+
+# calling & printing even numbers
+print(evenNumbers * 2)
+
