1f9400f6a1f3806938759bb98f0a4e58e086721d
Olivia Walker <79489152+Livvyx@users.noreply.github.com>
2022-05-05 09:23:39 +1200

    Add files via upload

---
+++ Python_Sets.py
@@ -0,0 +1,20 @@
+"""
+olivia Walker
+Set, tuples & lists Refresher
+"""
+
+print("\nCreating a set with tuples inside")
+# Creating a set with a tuple inside
+print("\n* * * * * * * * * * * * * * * * * ** ")
+mySet = {1, 'thesis', 9.81, (1, 2, 3), "a", "b", "c"}  # Tuple inside the set ()
+print(mySet)  # outputting the set
+
+print("\n* * * * * * * * * * * * * * * * * ** ")
+print("\nCreating a List")
+print("\n* * * * * * * * * * * * * * * * * ** ")
+# Creating a list
+myList = ["Olivia", "Sam", "Mum"]
+# Ouputting a list
+print(myList)
+
+print("\n* * * * * * * * * * * * * * * * * ** ")
